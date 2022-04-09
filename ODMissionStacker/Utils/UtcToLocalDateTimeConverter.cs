7db1439d4b4596e60d0a3a100cf33f38e21bae7f
WarmedxMints <a.timms@dsl.pipex.com>
2022-04-09 10:04:43 +0100

    v 1.5 Update
    Added splitter bar to adjust view.
    Fixed Mission expiry displaying incorrect time.

---
+++ ODMissionStacker/Utils/UtcToLocalDateTimeConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ODMissionStacker.Utils
+{
+    public class UtcToLocalDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return DateTime.MinValue;
+            }
+
+            if (value is DateTime time)
+            {
+                return time.ToLocalTime();
+            }
+
+            if (DateTime.TryParse(value?.ToString(), out DateTime parsedResult))
+            {
+                return parsedResult.ToLocalTime();
+            }
+
+            return DateTime.MinValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
